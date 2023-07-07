d12d4a591dc06c8aa4bbb0c29e534e514fdbc55f
alexandrabakhar <112278780+alexandrabakhar@users.noreply.github.com>
2023-07-08 00:12:14 +0300

    Логика открытия и закрытия модального окна при нажатии на card в profile 	modified:   src/components/Cards/Card/Card.jsx 	modified:   src/components/ModalSelectWorkout/ModalSelectWorkout.jsx

--- src/components/ModalSelectWorkout/ModalSelectWorkout.jsx
+++ src/components/ModalSelectWorkout/ModalSelectWorkout.jsx
@@ -1,9 +1,6 @@
 import s from './ModalSelectWorkout.module.scss';
-import { useState } from 'react';
 
-export const ModalSelectWorkout = () => {
-	const [isClose, setIsClose] = useState(true);
-
+export const ModalSelectWorkout = ({ isOpen, setIsOpen }) => {
 	//заглушка
 	const workouts = [
 		{
@@ -77,41 +74,43 @@ export const ModalSelectWorkout = () => {
 		},
 	];
 
-	return isClose ? null : (
-		<div
-			className={s.modal}
-			onClick={() => {
-				setIsClose(!isClose);
-			}}
-		>
+	return (
+		isOpen && (
 			<div
-				className={s.select__workout}
-				onClick={(e) => e.stopPropagation()}
+				className={s.modal}
+				onClick={() => {
+					setIsOpen(!isOpen);
+				}}
 			>
-				<h2 className={s.heading}>Выберите тренировку</h2>
-				<ul className={s.ul}>
-					{workouts.map((item, i) => {
-						const itemClass = item.completed
-							? `${s.item} ${s['item-completed']}`
-							: s.item;
+				<div
+					className={s.select__workout}
+					onClick={(e) => e.stopPropagation()}
+				>
+					<h2 className={s.heading}>Выберите тренировку</h2>
+					<ul className={s.ul}>
+						{workouts.map((item, i) => {
+							const itemClass = item.completed
+								? `${s.item} ${s['item-completed']}`
+								: s.item;
 
-						const number = i + 1;
+							const number = i + 1;
 
-						return (
-							<li key={number} className={s.li}>
-								<a href="/" className={itemClass}>
-									<h3 className={s.item__name}>
-										{item.name}
-									</h3>
-									<p className={s.item__text}>
-										{item.course} / {item.day}
-									</p>
-								</a>
-							</li>
-						);
-					})}
-				</ul>
+							return (
+								<li key={number} className={s.li}>
+									<a href="/" className={itemClass}>
+										<h3 className={s.item__name}>
+											{item.name}
+										</h3>
+										<p className={s.item__text}>
+											{item.course} / {item.day}
+										</p>
+									</a>
+								</li>
+							);
+						})}
+					</ul>
+				</div>
 			</div>
-		</div>
+		)
 	);
 };
