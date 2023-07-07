22bc0d8049f9e36d0f041450955ed3043dfc4685
distanceOn <distanceOn@yandex.ru>
2023-07-07 18:42:19 +0300

    Провести декомпозицию. Сделать Cards отдельным компонентом. Провести Рефакторинг в Cards и Card, добавить пропсы. Создать компонент MyCourses, завершить страницу Profile

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import s from "./App.module.scss";
 import Profile from "./pages/Profile/Profile";
-// import Main from './pages/Main/Main';
-// import UnauthorizedCourse from "./pages/unauthorizedCourse/UnauthorizedCourse";
-// import { ModalSubmittedProgress } from './components/ModalSubmittedProgress/ModalSubmittedProgress';
+import Main from "./pages/Main/Main";
+import UnauthorizedCourse from "./pages/unauthorizedCourse/UnauthorizedCourse";
+import { ModalSubmittedProgress } from "./components/ModalSubmittedProgress/ModalSubmittedProgress";
 
 function App() {
   return (
     <div className={s.App}>
-      {/* <Main />
-			<UnauthorizedCourse />
-			<ModalSubmittedProgress /> */}
+      <Main />
+      <UnauthorizedCourse />
+      <ModalSubmittedProgress />
       <Profile />
     </div>
   );
--- src/pages/Profile/MyCourses/MyCourses.jsx
+++ src/pages/Profile/MyCourses/MyCourses.jsx
@@ -1,9 +1,11 @@
+import Cards from "../../../components/Cards/Cards";
 import s from "./MyCourses.module.scss";
 
 const MyCourses = () => {
   return (
     <div className={s.courses}>
       <h2 className={s.courses__h2}>Мои курсы</h2>
+      <Cards page="profile" />
     </div>
   );
 };
