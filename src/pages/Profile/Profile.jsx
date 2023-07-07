ecf0a8533643ffc121286b745e5326dff5ea791f
distanceOn <distanceOn@yandex.ru>
2023-07-07 18:16:42 +0300

    Создать Info, добавить стили

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,10 +1,12 @@
 import Header from "./Header/Header";
+import Info from "./Info/Info";
 import s from "./Profile.module.scss";
 
 const Profile = () => {
   return (
     <div className={s.profile}>
       <Header />
+      <Info />
     </div>
   );
 };
