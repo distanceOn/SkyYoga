93075a0439d03db330a13e6413ac10e053604829
Maryia Bartnouskaya <maryiabartnouskaya@air-maryia.zte.com.cn>
2023-07-09 16:29:01 +0300

    исправления в верстке авторизованной страницы: объединила компоненты, поправила ключи'

--- src/pages/authorizedCourse/AuthorizedCourse.jsx
+++ src/pages/authorizedCourse/AuthorizedCourse.jsx
@@ -1,24 +1,18 @@
-import { Exercises } from "./Exercises/Exercises"
-import { Progress } from "./Progress/Progress"
-import { Title } from "./Title/Title"
-import { Video } from "./Video/Video"
-import { Workout } from "./selectedWorkout/Workout"
-import s from './AuthorizedCourse.module.scss'
+import { Exercises } from "./Exercises/Exercises";
+import { Progress } from "./Progress/Progress";
+
+import { Video } from "./Video/Video";
+
+import s from "./AuthorizedCourse.module.scss";
 
 export const AuthorizedCourse = () => {
-    return(
-        <div className={s.main}>
-        <Title/>
-        <Workout />
-        <Video/>
-        <div className={s.workout}>
-        <Exercises/>
-        <Progress/>
-        </div>
-        
-        </div>
-         
-        
-       
-    )
-}
+  return (
+    <div className={s.main}>
+      <Video />
+      <div className={s.workout}>
+        <Exercises />
+        <Progress />
+      </div>
+    </div>
+  );
+};
--- src/pages/authorizedCourse/Progress/Progress.jsx
+++ src/pages/authorizedCourse/Progress/Progress.jsx
@@ -1,35 +1,38 @@
-import s from './ProgressBar.module.scss'
-import ProgressBar from "@ramonak/react-progress-bar"
+import s from "./ProgressBar.module.scss";
+import ProgressBar from "@ramonak/react-progress-bar";
 
 export const Progress = () => {
-    const workouts = ['Наклоны вперед', 'Наклоны назад', 'Поднятие ног, согнутых в коленях']
-    const workoutNumb = [2]
-    return(
-        <section className={s.progress__container}>
-            
-            {workoutNumb.map((item)=>{
-                return(
-                    <h2 className={s.heading}>Мой прогресс по тренировке {item}:</h2>
-                )
-            })}
-            <div className={s.progress__workouts}>
-                {workouts.map((item)=>{
-                    return(
-                        <div className={s.progress__workouts_name}>
-                            <h3 className={s.progress__workouts_title}>{item}</h3>
-                            <ProgressBar  completed={45}
-                                className={s.wrapper}
-                                barContainerClassName={s.container}
-                                completedClassName={s.barCompleted}
-                                labelClassName={s.label}
-                                />
-                        </div>
-                    )
-                })}
+  const workouts = [
+    "Наклоны вперед",
+    "Наклоны назад",
+    "Поднятие ног, согнутых в коленях",
+  ];
+  const workoutNumb = [2];
+  return (
+    <section className={s.progress__container}>
+      {workoutNumb.map((item, i) => {
+        return (
+          <h2 key={i} className={s.heading}>
+            Мой прогресс по тренировке {item}:
+          </h2>
+        );
+      })}
+      <div className={s.progress__workouts}>
+        {workouts.map((item, i) => {
+          return (
+            <div key={i} className={s.progress__workouts_name}>
+              <h3 className={s.progress__workouts_title}>{item}</h3>
+              <ProgressBar
+                completed={45}
+                className={s.wrapper}
+                barContainerClassName={s.container}
+                completedClassName={s.barCompleted}
+                labelClassName={s.label}
+              />
             </div>
-           
-
-            
-        </section>
-    )
-}
+          );
+        })}
+      </div>
+    </section>
+  );
+};
