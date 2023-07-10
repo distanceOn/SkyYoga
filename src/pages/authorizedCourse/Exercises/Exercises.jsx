861a1e6147ddc434e708793191b7a181a831f751
alexandrabakhar <112278780+alexandrabakhar@users.noreply.github.com>
2023-07-10 13:19:36 +0300

    Логика появления ModalProgress при нажатии на "Заполнить свой прогресс" в authorizedCourse/Exercises;

--- src/pages/authorizedCourse/Exercises/Exercises.jsx
+++ src/pages/authorizedCourse/Exercises/Exercises.jsx
@@ -1,24 +1,35 @@
-import s from "./Exercises.module.scss";
+import s from './Exercises.module.scss';
+import { useState } from 'react';
+import { ModalProgress } from '../../../components/modalProgress/ModalProgress';
 
 export const Exercises = () => {
-  const exercisesTypes = [
-    "Наклон вперед (10 повторений)",
-    "Наклон назад (10 повторений)",
-    "Поднятие ног, согнутых в коленях (5 повторений)",
-  ];
-  return (
-    <section className={s.container}>
-      <h2 className={s.heading}>Упражнения</h2>
-      <ul className={s.ul}>
-        {exercisesTypes.map((item, i) => {
-          return (
-            <li key={i} className={s.li}>
-              {item}
-            </li>
-          );
-        })}
-      </ul>
-      <button className={s.button}>Заполнить свой прогресс</button>
-    </section>
-  );
+	const [isModalOpen, setIsModalOpen] = useState(false);
+
+	const exercisesTypes = [
+		'Наклон вперед (10 повторений)',
+		'Наклон назад (10 повторений)',
+		'Поднятие ног, согнутых в коленях (5 повторений)',
+	];
+	return (
+		<section className={s.container}>
+			<h2 className={s.heading}>Упражнения</h2>
+			<ul className={s.ul}>
+				{exercisesTypes.map((item, i) => {
+					return (
+						<li key={i} className={s.li}>
+							{item}
+						</li>
+					);
+				})}
+			</ul>
+			<button
+				onClick={() => setIsModalOpen(!isModalOpen)}
+				className={s.button}
+			>
+				Заполнить свой прогресс
+			</button>
+
+			<ModalProgress isOpen={isModalOpen} setIsOpen={setIsModalOpen} />
+		</section>
+	);
 };
