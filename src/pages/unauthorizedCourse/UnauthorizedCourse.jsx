e181e55e579cab3b51db9d4da8e70755d36ddff3
alexandrabakhar <112278780+alexandrabakhar@users.noreply.github.com>
2023-07-12 17:23:45 +0300

    Изменены названия компонентов в UnauthorizedCourse в соответствие с данными из бд;

--- src/pages/unauthorizedCourse/UnauthorizedCourse.jsx
+++ src/pages/unauthorizedCourse/UnauthorizedCourse.jsx
@@ -1,22 +1,27 @@
 import s from './UnauthorizedCourse.module.scss';
 
 import { Title } from './components/Title/Title';
-import { Description } from './components/Description/Description';
+import { Advantages } from './components/Advantages/Advantages';
 import { Directions } from './components/Directions/Directions';
-import { Summary } from './components/Summary/Summary';
+import { Description } from './components/Description/Description';
 import { Enrolling } from './components/Enrolling/Enrolling';
 import Header from './components/Header/Header';
 import { useParams } from 'react-router-dom';
+import { useGetCourseByNameQuery } from '../../redux/services/coursesApi';
 
 const UnauthorizedCourse = () => {
 	//все данные будут с бэка
 	const params = useParams();
-	const name = params.name;
+	const courseName = params.name;
+
+	const { data, isLoading } = useGetCourseByNameQuery(courseName);
+	if (isLoading) return <h1>Loading...</h1>;
+	console.log(data);
 
 	//тут будет запрос за данными на бэк по name или id (нужно решить)
 	//потом эти данные пойдут во все компоненты
 
-	if (name !== 'yoga') {
+	if (courseName !== 'yoga') {
 		return <h1>Здесь будет что-то кроме йоги</h1>;
 	}
 
@@ -24,9 +29,9 @@ const UnauthorizedCourse = () => {
 		<div className={s.container}>
 			<Header />
 			<Title />
-			<Description />
+			<Advantages />
 			<Directions />
-			<Summary />
+			<Description />
 			<Enrolling />
 		</div>
 	);
--- src/pages/unauthorizedCourse/components/Description/Description.jsx
+++ src/pages/unauthorizedCourse/components/Description/Description.jsx
@@ -1,27 +1,16 @@
 import s from './Description.module.scss';
 
-export const Description = () => {
-	//заглушка
-	const description = [
-		'Давно хотели попробовать йогу, но не решались начать.',
-		'Хотите укрепить позвоночник, избавиться от болей в спине и суставах.',
-		'Ищете активность, полезную для тела и души.',
-	];
 
+export const Description = () => {
 	return (
-		<section className="description">
-			<h2 className={s.heading}>Подойдет для вас, если:</h2>
-			<ul className={s.ul}>
-				{description.map((item, i) => {
-					const number = i + 1;
-					return (
-						<li key={i} className={s.li}>
-							<span className={s.number}>{number}</span>
-							<span>{item}</span>
-						</li>
-					);
-				})}
-			</ul>
+		<section className='description'>
+			<p className={s.text}>
+				Благодаря комплексному воздействию упражнений происходит
+				проработка всех групп мышц, тренировка суставов, улучшается
+				циркуляция крови. Кроме того, упражнения дарят отличное
+				настроение, заряжают бодростью и помогают противостоять
+				стрессам.
+			</p>
 		</section>
 	);
 };
