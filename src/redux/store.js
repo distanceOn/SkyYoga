9991abc7b0fc0f1ee930d4528f367e2fe3b2baeb
alexandrabakhar <112278780+alexandrabakhar@users.noreply.github.com>
2023-07-12 17:29:51 +0300

    Добавить предварительный userSlice;

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { coursesApi } from './services/coursesApi';
+import userReducer from './slices/user';
 
 export const store = configureStore({
 	reducer: {
+		user: userReducer,
 		[coursesApi.reducerPath]: coursesApi.reducer,
 	},
 
