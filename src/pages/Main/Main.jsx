252ba3a6ecf9d63e1718061a68bec54a1c5dfbd1
distanceOn <distanceOn@yandex.ru>
2023-07-14 22:18:37 +0300

    Объединить компоненты ModalNew... в компонент ModalNewAuth

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,20 +1,25 @@
-import Header from './Header/Header';
-import s from './Main.module.scss';
-import Inscription from './Inscription/Inscription';
-import Cards from '../../components/Cards/Cards';
-import Footer from './Footer/Footer';
+import Header from "./Header/Header";
+import s from "./Main.module.scss";
+import Inscription from "./Inscription/Inscription";
+import Cards from "../../components/Cards/Cards";
+import Footer from "./Footer/Footer";
+
+import { ModalNewAuth } from "../../components/ModalNewAuth/ModalNewAuth";
 
 const Main = () => {
-	return (
-		<div className={s.main}>
-			<div className={s.container}>
-				<Header />
-				<Inscription />
-				<Cards page="main" />
-				<Footer />
-			</div>
-		</div>
-	);
+  return (
+    <div className={s.main}>
+      <div className={s.container}>
+        <ModalNewAuth title="Новый логин:" />
+        <ModalNewAuth title="Новый пароль:" />
+
+        <Header />
+        <Inscription />
+        <Cards page="main" />
+        <Footer />
+      </div>
+    </div>
+  );
 };
 
 export default Main;
