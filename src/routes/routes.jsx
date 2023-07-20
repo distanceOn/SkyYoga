50eb3d084234fcb3273510566cc96cd8342ac64d
alexandrabakhar <112278780+alexandrabakhar@users.noreply.github.com>
2023-07-20 15:48:44 +0300

    Переименовать UnauthorizedCourse в Course;

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,28 +1,28 @@
-import { Routes, Route } from "react-router-dom";
-import Main from "../pages/Main/Main";
-import Profile from "../pages/Profile/Profile";
-import UnauthorizedCourse from "../pages/unauthorizedCourse/UnauthorizedCourse";
-import { AuthorizedCourse } from "../pages/authorizedCourse/AuthorizedCourse";
-import ProtectedRoute from "./ProtectedRoute";
-import { Auth } from "../pages/Auth/Auth";
+import { Routes, Route } from 'react-router-dom';
+import Main from '../pages/Main/Main';
+import Profile from '../pages/Profile/Profile';
+import Course from '../pages/Course/Course';
+import { AuthorizedCourse } from '../pages/authorizedCourse/AuthorizedCourse';
+import ProtectedRoute from './ProtectedRoute';
+import { Auth } from '../pages/Auth/Auth';
 
 const AppRoutes = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<Main />} />
-      {/* Приблизительные названия роутов. Когда будут готовы все страницы, изменить! */}
+	return (
+		<Routes>
+			<Route path="/" element={<Main />} />
+			{/* Приблизительные названия роутов. Когда будут готовы все страницы, изменить! */}
 
-      <Route path="/login" element={<Auth loginPage="true" />} />
-      <Route path="/registration" element={<Auth />} />
+			<Route path="/login" element={<Auth loginPage="true" />} />
+			<Route path="/registration" element={<Auth />} />
 
-      <Route path="/course/:name" element={<UnauthorizedCourse />} />
+			<Route path="/course/:name" element={<Course />} />
 
-      <Route element={<ProtectedRoute />}>
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/workout/:id" element={<AuthorizedCourse />} />
-      </Route>
-    </Routes>
-  );
+			<Route element={<ProtectedRoute />}>
+				<Route path="/profile" element={<Profile />} />
+				<Route path="/workout/:id" element={<AuthorizedCourse />} />
+			</Route>
+		</Routes>
+	);
 };
 
 export default AppRoutes;
