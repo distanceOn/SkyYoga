eabd24c9c8cf8b8ba64821bb6af9aba10c3ea22b
Maryia Bartnouskaya <maryiabartnouskaya@air-maryia.zte.com.cn>
2023-07-21 08:01:07 +0300

    настроен logout

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,34 @@
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import Logo from "../Logo/Logo";
 import ProfileIcon from "../ProfileIcon/ProfileIcon";
 import s from "./Header.module.scss";
 import Button from "../Button/Button";
+import { signOut } from "firebase/auth";
+import { auth } from "../../firebase/firebase";
 
 const Header = () => {
   const location = useLocation();
   const path = location.pathname;
+  const navigate = useNavigate();
+  const onLogout = () => {
+    signOut(auth)
+      .then(() => {
+        navigate("/");
+      })
+      .catch((error) => {
+        console.log("error");
+      });
+  };
 
   const showContent = () => {
     // пока что отображение контента зависит от того, где мы находимся, потом от факта авторизации
     if (path === "/profile" || path === "/workout/2") {
-      return <ProfileIcon />;
+      return (
+        <div>
+          <ProfileIcon />
+          <Button buttonText="Выйти" class={s.entry} onClick={onLogout} />
+        </div>
+      );
     } else {
       return <Button class={s.entry} buttonText="Войти" />;
     }
