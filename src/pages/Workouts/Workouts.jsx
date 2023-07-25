cad7444fd9436acb8fe9cb9a169c6a7b4696b017
alexandrabakhar <112278780+alexandrabakhar@users.noreply.github.com>
2023-07-25 22:59:00 +0300

    Переименовать Workout => Workouts;

--- src/pages/Workouts/Workouts.jsx
+++ src/pages/Workouts/Workouts.jsx
@@ -3,13 +3,13 @@ import { Progress } from "./Progress/Progress";
 
 import { Video } from "./Video/Video";
 
-import s from "./Workout.module.scss";
+import s from "./Workouts.module.scss";
 import Header from "../../components/Header/Header";
 import { useSelector } from "react-redux";
 import { useGetWorkoutByIdQuery } from "../../redux/services/workoutsApi";
 import { selectCurrentWorkout } from "../../redux/selectors";
 
-export const Workout = () => {
+export const Workouts = () => {
 	const workoutId = useSelector(selectCurrentWorkout);
 	!workoutId && console.log("Загрузка...");
 	const { data, isLoading } = useGetWorkoutByIdQuery(workoutId);
