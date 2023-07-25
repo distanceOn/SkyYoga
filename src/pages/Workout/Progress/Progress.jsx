076f5f6b5ca644354ff0a95f53fd21884cc8b79d
alexandrabakhar <112278780+alexandrabakhar@users.noreply.github.com>
2023-07-25 16:26:57 +0300

    Настроить разные цвета для ProgressBar;

--- src/pages/Workout/Progress/Progress.jsx
+++ src/pages/Workout/Progress/Progress.jsx
@@ -3,6 +3,7 @@ import ProgressBar from "@ramonak/react-progress-bar";
 import { useSelector } from "react-redux";
 import { selectCurrentWorkout, selectUserId } from "../../../redux/selectors";
 import { useGetUserProgressQuery } from "../../../redux/services/usersApi";
+// import { useRef } from "react";
 // import { useState } from "react";
 // import { useEffect } from "react";
 
@@ -10,15 +11,15 @@ export const Progress = ({ exercises, courseName }) => {
 	const userId = useSelector(selectUserId);
 	const workoutId = useSelector(selectCurrentWorkout);
 	// const [progress, setProgress] = useState(null);
-
 	const { data, isLoading } = useGetUserProgressQuery({
 		userId,
 		workoutId,
 		courseName,
 	});
 
-	if (isLoading) return console.log("Загрузка...");
+	const colors = ["#565eef", "#FF6D00", "#9A48F1", "#565eef", "#FF6D00", "#9A48F1"];
 
+	if (isLoading) return console.log("Загрузка...");
 
 	return (
 		<section className={s.progress__container}>
@@ -26,7 +27,7 @@ export const Progress = ({ exercises, courseName }) => {
 
 			<div className={s.progress__workouts}>
 				{exercises.map((item, i) => {
-					const uniqueClass = "barCompleted" + i;
+					const uniqueClass = "wrapper" + i;
 					const id = item["_id"];
 					console.log(id);
 
@@ -37,10 +38,14 @@ export const Progress = ({ exercises, courseName }) => {
 							</h3>
 							<ProgressBar
 								completed={data[id]}
-								className={s.wrapper}
+								className={`${s.wrapper} ${s[uniqueClass]}`}
 								barContainerClassName={s.container}
-								completedClassName={`${s.barCompleted} ${s[uniqueClass]}`}
+								bgColor={colors[i]}
+								customLabelStyles={`border: 2px solid ${colors[i]}`}
+								height="100%"
+								// completedClassName={`${s.barCompleted} ${s[uniqueClass]}`}
 								labelClassName={s.label}
+								borderRadius="2px black solid"
 							/>
 						</div>
 					);
