354544a93743bfb9a2f9d93ed1e29ab00b2969da
Maryia Bartnouskaya <maryiabartnouskaya@MacBook-Air-Maryia.local>
2023-07-25 05:56:26 +0200

    удалена лишний компонент workout,добавлено имя на  русскомЭ

--- src/pages/Workout/Video/Video.jsx
+++ src/pages/Workout/Video/Video.jsx
@@ -1,20 +1,21 @@
+/* eslint-disable indent */
 import s from "./Video.module.scss";
 import image from "../../../assets/imgs/playBtn.svg";
 
-export const Video = ({ src, name, course }) => {
-	return (
-		<div className={s.wrapper}>
-			<div className={s.title}>
-				<h1 className={s.title__text}>{course}</h1>
-			</div>
-			<div className={s.selections}>
-				<h2 className={s.li}>{name} </h2>
-			</div>
-			<div className={s.video__container}>
-				<video className={s.video} src={src}></video>
+export const Video = ({ src, name, courseName }) => {
+  return (
+    <div className={s.wrapper}>
+      <div className={s.title}>
+        <h2 className={s.title__text}>{courseName.ru}</h2>
+      </div>
+      <div className={s.selections}>
+        <h2 className={s.li}>{name} </h2>
+      </div>
+      <div className={s.video__container}>
+        <video className={s.video} src={src}></video>
 
-				<img className={s.play__button} width="156" src={image} alt="play" />
-			</div>
-		</div>
-	);
+        <img className={s.play__button} width="156" src={image} alt="play" />
+      </div>
+    </div>
+  );
 };
--- src/pages/Workout/Workout.jsx
+++ src/pages/Workout/Workout.jsx
@@ -1,31 +1,30 @@
+/* eslint-disable indent */
 import { Exercises } from "./Exercises/Exercises";
 import { Progress } from "./Progress/Progress";
 
 import { Video } from "./Video/Video";
 
 import s from "./Workout.module.scss";
-// import { useParams } from "react-router-dom";
+
 import Header from "../../components/Header/Header";
 import { useSelector } from "react-redux";
 import { useGetWorkoutByIdQuery } from "../../redux/services/workoutsApi";
 
 export const Workout = () => {
+  const workoutId = useSelector((state) => state.user.currentWorkout);
+  !workoutId && console.log("Загрузка...");
+  const { data, isLoading } = useGetWorkoutByIdQuery(workoutId);
+  if (isLoading) return console.log("Загрузка...");
+  const { src, courseName, exercises, name } = data;
 
-	const workoutId = useSelector((state) => state.user.currentWorkout);
-	!workoutId && console.log("Загрузка...");
-	const { data, isLoading } = useGetWorkoutByIdQuery(workoutId);
-	if (isLoading) return console.log("Загрузка...");
-	const {src, course, exercises, name, } = data;
-	
-
-	return (
-		<div className={s.main}>
-			<Header />
-			<Video src={src} name={name} course={course}/>
-			<div className={s.workout}>
-				<Exercises exercises={exercises}/>
-				<Progress exercises={exercises}/>
-			</div>
-		</div>
-	);
+  return (
+    <div className={s.main}>
+      <Header />
+      <Video src={src} name={name} courseName={courseName} />
+      <div className={s.workout}>
+        <Exercises exercises={exercises} />
+        <Progress exercises={exercises} />
+      </div>
+    </div>
+  );
 };
