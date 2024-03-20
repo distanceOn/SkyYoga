33781eeae0398058675fab32481f4b22fa09acf4
Roman Kondrashev <distanceOn@yandex.ru>
2024-03-21 01:54:42 +0300

    fix: resolve problem with names

--- src/pages/Auth/components/AuthLogin/AuthLogin.jsx
+++ src/pages/Auth/components/AuthLogin/AuthLogin.jsx
@@ -7,113 +7,109 @@ import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../../../firebase/firebase";
 
 export const AuthLogin = ({
-	navigate,
-	dispatch,
-	setLogin,
-	setIsError,
-	setIsVisiblePopup,
-	setErrorState,
-	setEmail,
-	errorSource,
-	targetPopup,
-	setPassword,
-	targetPassword,
-	observeToError,
-	popupPosition,
-	errorText,
-	isError,
-	isVisiblePopup,
-	email,
-	password,
+  navigate,
+  dispatch,
+  setLogin,
+  setIsError,
+  setIsVisiblePopup,
+  setErrorState,
+  setEmail,
+  errorSource,
+  targetPopup,
+  setPassword,
+  targetPassword,
+  observeToError,
+  popupPosition,
+  errorText,
+  isError,
+  isVisiblePopup,
+  email,
+  password,
 }) => {
-	const handleRegistrationButtonClick = () => {
-		navigate("/registration");
-	};
+  const handleRegistrationButtonClick = () => {
+    navigate("/registration");
+  };
 
-	async function onSubmit(event) {
-		event.preventDefault();
-		await signInWithEmailAndPassword(auth, email, password)
-			.then((userData) => {
-				const user = userData.user;
+  async function onSubmit(event) {
+    event.preventDefault();
+    await signInWithEmailAndPassword(auth, email, password)
+      .then((userData) => {
+        const user = userData.user;
 
-				dispatch(
-					setLogin({
-						userId: user.uid,
-						email: user.email,
-					})
-				);
-				localStorage.setItem("userID", user.uid);
-				localStorage.setItem("userEmail", user.email);
-				navigate("/profile");
-			})
-			.catch((error) => {
-				// устанавливаем isError когда ошибка
-				setIsError(true);
-				setIsVisiblePopup(true);
-				const errorCode = error.code;
-				// устанавливаем состояние ошибки
-				setErrorState(errorCode);
-			});
-	}
-	return (
-		<div className={s.container}>
-			<div className={s.modal}>
-				<div className={s.login}>
-					<Logo />
-					<div className={s.login__margin_top}>
-						<Input
-							placeholderText="Логин"
-							id="email-address"
-							name="email"
-							type="email"
-							onChange={(e) => {
-								setEmail(e.target.value);
-								if (errorSource === targetPopup) {
-									observeToError();
-								}
-							}}
-							ref={targetPopup}
-						/>
-						<Input
-							placeholderText="Пароль"
-							id="password"
-							name="password"
-							type="password"
-							onChange={(e) => {
-								setPassword(e.target.value);
-								if (errorSource === targetPassword) {
-									observeToError();
-								}
-							}}
-							ref={targetPassword}
-						/>
-					</div>
+        dispatch(
+          setLogin({
+            userId: user.uid,
+            email: user.email,
+          })
+        );
+        localStorage.setItem("userID", user.uid);
+        localStorage.setItem("userEmail", user.email);
+        navigate("/profile");
+      })
+      .catch((error) => {
+        // устанавливаем isError когда ошибка
+        setIsError(true);
+        setIsVisiblePopup(true);
+        const errorCode = error.code;
+        // устанавливаем состояние ошибки
+        setErrorState(errorCode);
+      });
+  }
+  return (
+    <div className={s.container}>
+      <div className={s.modal}>
+        <div className={s.login}>
+          <Logo />
+          <div className={s.login__margin_top}>
+            <Input
+              placeholderText="E-mail"
+              id="email-address"
+              name="email"
+              type="email"
+              onChange={(e) => {
+                setEmail(e.target.value);
+                if (errorSource === targetPopup) {
+                  observeToError();
+                }
+              }}
+              ref={targetPopup}
+            />
+            <Input
+              placeholderText="Пароль"
+              id="password"
+              name="password"
+              type="password"
+              onChange={(e) => {
+                setPassword(e.target.value);
+                if (errorSource === targetPassword) {
+                  observeToError();
+                }
+              }}
+              ref={targetPassword}
+            />
+          </div>
 
-					<div className={s.login__margin}>
-						<Button
-							buttonText="Войти"
-							type="submit"
-							onClick={onSubmit}
-						/>
-					</div>
-					<button
-						className={s.btn__register}
-						type="submit"
-						onClick={handleRegistrationButtonClick}
-					>
-						Зарегистрироваться
-					</button>
-					<AuthPopup
-						style={{
-							top: popupPosition.top,
-							left: popupPosition.left,
-							opacity: isError ? 1 : 0,
-						}}
-						errorText={errorText}
-						isVisiblePopup={isVisiblePopup}
-					/>
-				</div>
-			</div>
-		</div>
-	);
+          <div className={s.login__margin}>
+            <Button buttonText="Войти" type="submit" onClick={onSubmit} />
+          </div>
+          <button
+            className={s.btn__register}
+            type="submit"
+            onClick={handleRegistrationButtonClick}
+          >
+            Зарегистрироваться
+          </button>
+          <AuthPopup
+            style={{
+              top: popupPosition.top,
+              left: popupPosition.left,
+              opacity: isError ? 1 : 0,
+            }}
+            errorText={errorText}
+            isVisiblePopup={isVisiblePopup}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
--- src/pages/Auth/components/AuthRegistration/AuthRegistration.jsx
+++ src/pages/Auth/components/AuthRegistration/AuthRegistration.jsx
@@ -8,117 +8,117 @@ import { useAddNewUserMutation } from "../../../../redux/services/usersApi";
 import { userData } from "../../userData/userData";
 
 export const AuthRegistration = ({
-	password,
-	repeatPassword,
-	setIsError,
-	setErrorState,
-	auth,
-	email,
-	navigate,
-	setIsVisiblePopup,
-	setEmail,
-	errorSource,
-	observeToError,
-	targetPopup,
-	targetPassword,
-	setPassword,
-	setRepeatPassword,
-	targetRepeat,
-	popupPosition,
-	isError,
-	errorText,
-	isVisiblePopup,
+  password,
+  repeatPassword,
+  setIsError,
+  setErrorState,
+  auth,
+  email,
+  navigate,
+  setIsVisiblePopup,
+  setEmail,
+  errorSource,
+  observeToError,
+  targetPopup,
+  targetPassword,
+  setPassword,
+  setRepeatPassword,
+  targetRepeat,
+  popupPosition,
+  isError,
+  errorText,
+  isVisiblePopup,
 }) => {
-	const [addNewUser] = useAddNewUserMutation();
+  const [addNewUser] = useAddNewUserMutation();
 
-	const createUser = async (uid) => {
-		await addNewUser(userData(uid));
-	};
+  const createUser = async (uid) => {
+    await addNewUser(userData(uid));
+  };
 
-	const onLogin = (e) => {
-		e.preventDefault();
+  const onLogin = (e) => {
+    e.preventDefault();
 
-		if (password !== repeatPassword) {
-			setIsError(true);
-			setErrorState("passwords-mismatch");
-			return;
-		}
+    if (password !== repeatPassword) {
+      setIsError(true);
+      setErrorState("passwords-mismatch");
+      return;
+    }
 
-		createUserWithEmailAndPassword(auth, email, password)
-			.then((userData) => {
-				const user = userData.user;
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userData) => {
+        const user = userData.user;
 
-				createUser(user.uid);
-				navigate("/login");
-			})
-			.catch((error) => {
-				// устанавливаем isError когда ошибка
-				setIsError(true);
-				setIsVisiblePopup(true);
-				const errorCode = error.code;
-				// устанавливаем состояние ошибки
-				setErrorState(errorCode);
-			});
-	};
-	return (
-		<div className={s.container}>
-			<div className={s.modal}>
-				<div className={s.login}>
-					<Logo />
-					<div className={s.login__margin_top}>
-						<Input
-							placeholderText="Логин"
-							id="email-address"
-							name="email"
-							type="email"
-							onChange={(e) => {
-								setEmail(e.target.value);
-								if (errorSource === targetPopup) {
-									observeToError();
-								}
-							}}
-							ref={targetPopup}
-						/>
-						<Input
-							placeholderText="Пароль"
-							id="password"
-							name="password"
-							type="password"
-							onChange={(e) => {
-								setPassword(e.target.value);
-								if (errorSource === targetPassword) {
-									observeToError();
-								}
-							}}
-							ref={targetPassword}
-						/>
-						<Input
-							placeholderText="Повторите пароль"
-							type="password"
-							onChange={(e) => {
-								setRepeatPassword(e.target.value);
-								if (errorSource === targetRepeat) {
-									observeToError();
-								}
-							}}
-							ref={targetRepeat}
-						/>
-					</div>
+        createUser(user.uid);
+        navigate("/login");
+      })
+      .catch((error) => {
+        // устанавливаем isError когда ошибка
+        setIsError(true);
+        setIsVisiblePopup(true);
+        const errorCode = error.code;
+        // устанавливаем состояние ошибки
+        setErrorState(errorCode);
+      });
+  };
+  return (
+    <div className={s.container}>
+      <div className={s.modal}>
+        <div className={s.login}>
+          <Logo />
+          <div className={s.login__margin_top}>
+            <Input
+              placeholderText="E-mail"
+              id="email-address"
+              name="email"
+              type="email"
+              onChange={(e) => {
+                setEmail(e.target.value);
+                if (errorSource === targetPopup) {
+                  observeToError();
+                }
+              }}
+              ref={targetPopup}
+            />
+            <Input
+              placeholderText="Пароль"
+              id="password"
+              name="password"
+              type="password"
+              onChange={(e) => {
+                setPassword(e.target.value);
+                if (errorSource === targetPassword) {
+                  observeToError();
+                }
+              }}
+              ref={targetPassword}
+            />
+            <Input
+              placeholderText="Повторите пароль"
+              type="password"
+              onChange={(e) => {
+                setRepeatPassword(e.target.value);
+                if (errorSource === targetRepeat) {
+                  observeToError();
+                }
+              }}
+              ref={targetRepeat}
+            />
+          </div>
 
-					<div className={s.login__margin}>
-						<Button buttonText="Зарегистрироваться" onClick={onLogin} />
-					</div>
-					<AuthPopup
-						style={{
-							top: popupPosition.top,
-							left: popupPosition.left,
-							opacity: isError ? 1 : 0,
-						}}
-						errorText={errorText}
-						isVisiblePopup={isVisiblePopup}
-					/>
-				</div>
-			</div>
-		</div>
-	);
+          <div className={s.login__margin}>
+            <Button buttonText="Зарегистрироваться" onClick={onLogin} />
+          </div>
+          <AuthPopup
+            style={{
+              top: popupPosition.top,
+              left: popupPosition.left,
+              opacity: isError ? 1 : 0,
+            }}
+            errorText={errorText}
+            isVisiblePopup={isVisiblePopup}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
